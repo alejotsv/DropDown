1be59c495aa1dde394c79cc218282191620589b7
Alejandro Salas <asalas.wd@outlook.com>
2022-11-11 14:56:23 -0500

    Add label before drop down

--- src/com/java21days/CreateDropDown.java
+++ src/com/java21days/CreateDropDown.java
@@ -2,6 +2,7 @@ package com.java21days;
 
 public class CreateDropDown {
     public static void main(String[] args) {
-        System.out.println("This is my main class");
+        String[] options = { "Atom", "RSS 0.92", "RSS 1.0", "RSS 2.0" };
+        DropDown dropDown = new DropDown("Choose an Output Format", "Choose an output format", options);
     }
 }
--- src/com/java21days/DropDown.java
+++ src/com/java21days/DropDown.java
@@ -4,12 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DropDown extends JFrame {
-    Dimension dim = new Dimension(200,200);
+    Dimension dim = new Dimension(300,200);
 
     DropDown(String title, String message, String[] options){
         super(title);
+        setSize(dim);
         JPanel panel = new JPanel();
+        JLabel messageLabel = new JLabel(message);
 
+
+        panel.add(messageLabel);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(panel);
