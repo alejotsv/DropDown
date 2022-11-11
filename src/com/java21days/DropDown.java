03469da45996df60482f49e9b5067a86191627b9
Alejandro Salas <asalas.wd@outlook.com>
2022-11-11 14:51:42 -0500

    Create DropDown class with constructor

--- src/com/java21days/DropDown.java
+++ src/com/java21days/DropDown.java
@@ -1,4 +1,18 @@
 package com.java21days;
 
-public class DropDown {
+import javax.swing.*;
+import java.awt.*;
+
+public class DropDown extends JFrame {
+    Dimension dim = new Dimension(200,200);
+
+    DropDown(String title, String message, String[] options){
+        super(title);
+        JPanel panel = new JPanel();
+
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(panel);
+        setVisible(true);
+    }
 }
